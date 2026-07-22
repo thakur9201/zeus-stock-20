fa3eb4c8e8154676d975f325418e7b146f2c9c9a
thakur9201 <kanhaiyathakur303@gmail.com>
2026-07-22 14:19:57 +0530

    Send Telegram message on script start

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("ZEUS-STOCK-20 Started")
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": "✅ ZEUS-STOCK-20 Test Successful!"
+})
+
+print("Message Sent")
